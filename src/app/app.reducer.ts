721aff496c3451eb96d122b9abdce64b8009dc1e
Kevin Lam <kevinlam.ucsb@gmail.com>
2017-04-17 12:13:01 -0700

    added boilerplate store and rootReducer

---
+++ src/app/app.reducer.ts
@@ -0,0 +1,11 @@
+import { Reducer, combineReducers } from 'redux';
+
+export interface AppState {
+
+}
+
+const rootReducer: Reducer<AppState> = combineReducers<AppState>({
+
+});
+
+export default rootReducer;
