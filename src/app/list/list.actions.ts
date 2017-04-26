bead1d70a8e148078fb65356058e8d66ccdffbc8
Robert Murray <work@robertmurray.me>
2017-04-26 07:48:29 -0700

    added some comments

--- src/app/list/list.actions.ts
+++ src/app/list/list.actions.ts
@@ -1,6 +1,7 @@
 import { Action, ActionCreator } from 'redux';
 import { List } from './list.model';
 
+// this interface shouldn't be here. It should be in it's own file or in a file with other interfaces.
 export interface DoSearchAction extends Action {
   payload: List;
 }
--- src/app/list/list.model.ts
+++ src/app/list/list.model.ts
@@ -1,3 +1,5 @@
+
+// an interface and a model aren't the same. This doesn't belong in this file if there is no model.
 export interface List {
   exercises: null;
 }
